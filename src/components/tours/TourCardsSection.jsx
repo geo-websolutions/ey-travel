a14af0a89b66bc7250436f22f0d2c29a4f5e9a45
geo-websolutions <geowebsolutions1@gmail.com>
2025-08-08 17:06:41 +0300

    fixed tour type array in firestore structure

--- src/components/tours/TourCardsSection.jsx
+++ src/components/tours/TourCardsSection.jsx
@@ -67,7 +67,16 @@ export default function TourCardsSection({ tours, city, slug }) {
               />
               {/* Tour Type */}
               <div className="absolute bottom-4 right-4 bg-stone-900/80 text-white text-xs px-2 py-1 rounded">
-                {formatToTitleCase(tour.basicInfo.type)}
+                {console.log(tour.basicInfo.type)}
+                {(Array.isArray(tour.basicInfo.type) ? tour.basicInfo.type : [tour.basicInfo.type])
+                  .map((type, i) => (
+                    <span 
+                      key={i} 
+                      className="bg-stone-900/80 text-white text-xs px-2 py-1 rounded"
+                    >
+                      {formatToTitleCase(type)}
+                    </span>
+                  ))}
               </div>
             </Link>
 
